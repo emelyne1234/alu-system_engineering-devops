eb2efe49ede45e864921d24cc93e7d1c1e41b34f
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-09-16 06:45:52 -0700

    puppet

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,4 @@
-#!/usr/bin/env bash
-# making changes to our configuration using puppet
-
-authenticationpassword no
-ip 54.234.226.73
-user ubuntu
-
+# making changes to our configuration using puppet 
 exec { 'echo':
 	"PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
         path    => '/bin/'
