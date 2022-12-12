3a7dfb826d74be406d83bde92a4c8db8d68b130e
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-12-12 02:51:10 -0800

    strace

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 #The puppet script is fixing 500 error
 exec { '/var/www/html/wp-settings.php':
 command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
-path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
